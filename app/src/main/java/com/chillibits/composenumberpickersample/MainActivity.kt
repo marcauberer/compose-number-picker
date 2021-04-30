9c9fd6051254ff9c0c6b94087cddbd713d52fa51
Marc Auberer <marc.auberer@sap.com>
2021-04-30 09:48:07 +0200

    Add VerticalNumberPicker

--- app/src/main/java/com/chillibits/composenumberpickersample/MainActivity.kt
+++ app/src/main/java/com/chillibits/composenumberpickersample/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.chillibits.composenumberpickersample
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.layoutId
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.core.view.WindowCompat
 import com.chillibits.composenumberpicker.HorizontalNumberPicker
+import com.chillibits.composenumberpicker.VerticalNumberPicker
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +37,27 @@ class MainActivity : AppCompatActivity() {
                     },
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    HorizontalNumberPicker(min = 10, max = 100, default = 50, modifier = Modifier.layoutId("layout"))
+                    Column(
+                        modifier = Modifier.layoutId("picker"),
+                        horizontalAlignment = CenterHorizontally
+                    ) {
+                        Text(
+                            text = getString(R.string.horizontal_picker),
+                            modifier = Modifier.width(IntrinsicSize.Max).align(CenterHorizontally))
+                        HorizontalNumberPicker(
+                            min = 10,
+                            max = 100,
+                            default = 50
+                        )
+                        Text(
+                            text = getString(R.string.vertical_picker),
+                            modifier = Modifier.width(IntrinsicSize.Max).align(CenterHorizontally))
+                        VerticalNumberPicker(
+                            min = 20,
+                            max=30,
+                            default = 20
+                        )
+                    }
                 }
             }
         }
--- numberpicker/src/main/java/com/chillibits/composenumberpicker/VerticalNumberPicker.kt
+++ numberpicker/src/main/java/com/chillibits/composenumberpicker/VerticalNumberPicker.kt
@@ -1,52 +1,52 @@
 package com.chillibits.composenumberpicker
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@Preview(name="Horizontal number picker")
+@Preview(name = "Vertical number picker")
 @Composable
-fun HorizontalNumberPicker(
+fun VerticalNumberPicker(
     modifier: Modifier = Modifier,
-    height: Dp = 45.dp,
+    width: Dp = 45.dp,
     min: Int = 0,
     max: Int = 10,
     default: Int = min
 ) {
     val number = remember { mutableStateOf(default) }
 
-    Row {
+    Column {
         PickerButton(
-            size = height,
-            drawable = R.drawable.ic_arrow_left,
+            size = width,
+            drawable = R.drawable.ic_arrow_up,
             onClick = {
-                if (number.value > min) number.value--
+                if (number.value < max) number.value++
             }
         )
         Text(
             text = number.value.toString(),
-            fontSize = (height.value / 2).sp,
+            fontSize = (width.value / 2).sp,
             modifier = Modifier.padding(10.dp)
-                .height(IntrinsicSize.Max)
-                .align(CenterVertically)
+                .width(IntrinsicSize.Max)
+                .align(CenterHorizontally)
         )
         PickerButton(
-            size = height,
-            drawable = R.drawable.ic_arrow_right,
+            size = width,
+            drawable = R.drawable.ic_arrow_down,
             onClick = {
-                if (number.value < max) number.value++
+                if (number.value > min) number.value--
             }
         )
     }
